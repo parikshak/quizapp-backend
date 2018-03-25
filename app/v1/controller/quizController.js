97720fb9933de6b37ef37199f573927dd304fa39
saketvikram <saket.singh@tarento.com>
2018-03-25 19:32:08 +0530

    keycloak setup

--- app/v1/controller/quizController.js
+++ app/v1/controller/quizController.js
@@ -1,9 +1,59 @@
 
 /* quizController */
+var quizdata = {
+  "Test": {
+    "part": [
+      {
+        "id": "1",
+        "heading": "Sample part 1 heading",
+        "question": [
+          {
+            "title": "Sample Question in part 1",
+            "answers": {
+              "answer": [
+                {
+                  "@label": "Option answer 1",
+                  "@value": "J"
+                },
+                {
+                  "@label": "Option answer 2",
+                  "@value": "P"
+                }
+              ]
+            }
+          }
+        ]
+      },
+      {
+        "id": "2",
+        "heading": "Sample part 2 heading",
+        "question": [
+          {
+            "title": "Sample Question in part 2",
+            "answers": {
+              "answer": [
+                {
+                  "@label": "Option answer 1",
+                  "@value": "J"
+                },
+                {
+                  "@label": "Option answer 2",
+                  "@value": "P"
+                }
+              ]
+            }
+          }
+        ]
+      }
+    ]
+  }
+};
 
 var quizController = {
+
+
   loadQuestions: function (req, res, next) {
-    res.json({data: 'success'})
+    res.json({data: quizdata})
   }
 }
 
--- server.js
+++ server.js
@@ -1,24 +1,54 @@
 var express = require('express');
 var helmet = require('helmet');
 var bodyparser = require('body-parser');
-
 var dbConnection = require('./services/dbConnection');
+var app = express();
+const Keycloak = require('keycloak-connect');
+const session = require('express-session');
 
-var app = express()
+//session
+app.use(session({
+  secret:'we243fnkjsdhcjksd',
+  resave: false,
+  saveUninitialized: true,
+  store: memoryStore
+}));
 
-app.use(helmet())
+var memoryStore = new session.MemoryStore();
+var keycloak = new Keycloak({ store: memoryStore });
+var userController = require('./app/v1/controller/userController');
+var quizController = require('./app/v1/controller/quizController');
+app.use(keycloak.middleware());
+
+//route protected with Keycloak
+app.get('/', keycloak.protect(), function(req, res){
+  res.json('Welcome to Backend App.Please login');
+});
+app.get('/api/v1/', userController.login); // To test this url is unprotected
+app.get('/api/v1/loadQuestionst', quizController.loadQuestions);
+app.get('/api/v1/loadQuestions',keycloak.protect(),  quizController.loadQuestions);
+
+app.post('/api/v1/login', keycloak.protect(), userController.login);
+
+//route protected with Keycloak
+app.get('/test', keycloak.protect(), function(req, res){
+  res.json({title:'Test of the test'});
+});
+app.use( keycloak.middleware( { logout: '/logout'} ));
+
+
 
 dbConnection.DBConnectMongoose().then(() => {
   var routes = require('./app/routes');
 
-  app.use(bodyparser.urlencoded({extended: true}));
-  app.use(bodyparser.json({limit: '3mb'}));
+app.use(bodyparser.urlencoded({extended: true}));
+app.use(bodyparser.json({limit: '3mb'}));
 
-  routes.assignRoutes(app);
+routes.assignRoutes(app);
 
-  app.listen(3000);
+app.listen(3000);
 
-  console.log('Server listening on port 3000');
+console.log('Server listening on port 3000');
 })
 .catch(err => {
   console.log('Error: ' + err)
